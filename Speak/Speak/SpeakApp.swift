8b84ccefa0ec6c5305b7f86d4951c4cfa175b188
Richard Velasquez <ravelasuez99@gmail.com>
2024-03-12 13:26:58 -0700

    Added out of the box Xcode project

---
+++ Speak/Speak/SpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakApp.swift
+//  Speak
+//
+//  Created by Richard Velasquez on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
