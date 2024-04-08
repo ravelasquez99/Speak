f492aca1f81010e34ca765911d10f440364f7435
Richard Velasquez <ravelasuez99@gmail.com>
2024-04-08 11:16:45 -0700

    Added Heaps

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+
+
+
+/**
+size input integer
+ use cases
+ assumptions
+- input > 0
+- n columns n rows
+- spiral clockwise
+- array of arrays
+-
+
+3
+123
+894
+765
+
+1. build the array of arrays
+2. traverse the matrix in spiral order inserting the next number
+*/
